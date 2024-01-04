bfae77a08c910f765716b989097aaf1cffb96607
dbagunu <dcmbagunu@tutanota.com>
2024-01-04 21:11:44 +0800

    Changed installation steps according to guide

--- userdata/jenkins-setup.sh
+++ userdata/jenkins-setup.sh
@@ -2,9 +2,10 @@
 sudo apt update
 sudo apt install openjdk-11-jdk -y
 sudo apt install maven -y
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
-  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-  
+
+# Updated installation steps from guide
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
